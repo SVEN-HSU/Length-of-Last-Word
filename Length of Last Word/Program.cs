79b63179bea35e89af41337df2a8b2cb571fedc2
SVEN-HSU <sven.hsu@gmail.com>
2017-01-15 19:40:13 +0800

    20170115

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,41 @@
+﻿//https://leetcode.com/problems/length-of-last-word/
+//Accepted
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " ";
+            Program p = new Program();
+            Console.WriteLine("LengthOfLastWord=" + p.LengthOfLastWord(s));
+            Console.Read();
+        }
+
+        public int LengthOfLastWord(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            string[] chunks = s.Split(' ');
+            //Console.WriteLine("LastWord=" + chunks.Last());
+
+            if (chunks.Any(x => x.Length > 0))
+            {
+                return chunks.Where(x => x.Length > 0).Last().Length;
+            }
+            else
+            {
+                return 0;
+            }            
+        }
+    }
+}
